42d2c0f392f7bc204f2212b4cc877f8f5b96a0d0
tasyrikanurul <100851839+tasyrikanurul@users.noreply.github.com>
2022-04-15 23:23:07 +0700

    menambah fitur view sale purchase damage dengan parameterization/generics

---
+++ StationaryShopManagement/src/StationaryShopManagement/View Sale Purchase Damage/Purchase.java
@@ -0,0 +1,21 @@
+//Tasyrika Nurul Hajar - 1302201131
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view.sale.purchase.damage;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Purchase<T>{
+    private T purchase;
+    
+    public void Purchase (T purchase)
+    {
+        this.purchase=purchase;
+    }
+}
+
