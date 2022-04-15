804dc237d5cd6bb21d1b99a0b9c11a187be85251
Xiraj <akbarsirajuddinh@gmail.com>
2022-04-16 00:32:16 +0700

    add parsing feature

--- StationaryShopManagement/src/purchaseEntry/prchseEntry.java
+++ StationaryShopManagement/src/purchaseEntry/prchseEntry.java
@@ -1,28 +1,45 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package purchaseEntry;
 
-/**
- *
- * @author akbar
- */
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+
+import org.json.simple.*;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 public class prchseEntry {
-        public static void main(String[] args) {
-        System.out.println("================================");
-        System.out.println("-----------Purchase----------");
-        System.out.println("================================");
-        // Scanner inputData = new Scanner(System.in);
-        // System.out.print("Masukkan nama barang: ");
-        // String str = inputData.nextLine();
-        // listItem(Item);
-        String item[] = {
-                "Nama Produk", "Jumlah Pembelian", "Nama Vendor", "Jumlah Tagihan Total",
-        };
-        for (int i = 0; i <= item.length; i++) {
-            System.out.println(item[i]);
+
+    public static void ReadJSON() {
+        try {
+            Object obj = new JSONParser().parse(new FileReader("D:\\Kuliah\\Semester 4\\Konstruksi Perangkat Lunak\\Stationary-Shop-Management\\StationaryShopManagement\\src\\purchaseEntry\\purchasedata.json"));
+            JSONArray jsonarray = (JSONArray) obj;
+            Iterator itr = jsonarray.iterator();
+            System.out.println("Purchase");
+            int i = 1;
+            while (itr.hasNext()) {
+                JSONObject atr = (JSONObject) itr.next();
+                System.out.println("Nama Produk :"+atr.get("nama_produk"));
+                System.out.println("Jumlah Pembelian :"+atr.get("jml_pembelian"));
+                System.out.println("Harga :"+atr.get("harga"));
+                System.out.println("Nama Vendor :"+atr.get("nama_vendor"));
+                System.out.println("Jumlah Tagihan Total :"+atr.get("jml_total_tagihan"));
+                i +=1;
+            }
+
+        } catch (FileNotFoundException FE) {
+            System.out.println("File tidak ditemukan, periksa ulang path dari file json kembali!");
+            FE.printStackTrace();
+        } catch (IOException IE) {
+            System.out.println("Error "+IE);
+            IE.printStackTrace();
+        } catch (ParseException e) {
+            System.out.println("Gagal parsing");
+            e.printStackTrace();
         }
+    }
+    public static void main(String[] args) {
+        prchseEntry.ReadJSON();
     }
 }
