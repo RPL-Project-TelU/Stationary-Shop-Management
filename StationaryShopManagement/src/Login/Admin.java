287e6eb5860ee67cab101c579f9fefcededb4eeb
Ahmadnaufal2525 <ahmadnaufal588@gmail.com>
2022-04-23 11:45:27 +0700

    Membuat fitur View dan Setup Produk Menggunakan Table Driven

--- StationaryShopManagement/src/Login/Admin.java
+++
@@ -1,9 +0,0 @@
-
-package Login;
-//Afif 1302204076
-
-public class Admin {
-    public static void Admin(String user, String pass){
-        System.out.println("Login " + user + " Berhasil");   
-    }
-}
