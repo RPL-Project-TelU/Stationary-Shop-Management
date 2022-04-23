5e6eafe822757ce254da4193b4d349f8b2eab079
Ahmadnaufal2525 <ahmadnaufal588@gmail.com>
2022-04-23 09:12:59 +0700

    menambahkan fitur login dengan automata

---
+++ StationaryShopManagement/src/Login/Admin.java
@@ -0,0 +1,9 @@
+
+package Login;
+//Afif 1302204076
+
+public class Admin {
+    public static void Admin(String user, String pass){
+        System.out.println("Login " + user + " Berhasil");   
+    }
+}
